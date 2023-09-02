e692be01aa93b23073f1f439f4b0564b35d573fd
Tim Loden <tim.m.loden@gmail.com>
2023-09-01 20:02:02 -0700

    added recoleta font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,37 @@
+import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 import '@/styles/globals.css'
 
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+const recoleta = localFont({
+  src: [
+    {
+      path: '../public/fonts/recoleta-regular-webfont.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/recoleta-medium-webfont.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/recoleta-semibold-webfont.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-recoleta',
+})
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <main className={`${inter.variable} ${recoleta.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
