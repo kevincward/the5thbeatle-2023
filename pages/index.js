18d708ab9ba7dc9d3a946949c0ffbc9d2f140237
Tim Loden <tim.m.loden@gmail.com>
2023-09-23 13:58:13 -0700

    added homepage hero text

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-    return <main className={``}></main>;
+    return (
+        <main className='py-20'>
+            <div className='container'>
+                <div className='flex'>
+                    <div className='lg:basis-1/2'>
+                        <h1>I am the 5th Beatle.</h1>
+                        <p>
+                            While my band is on hiatus, I&apos;m focused on
+                            designing meaningful products.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 }
