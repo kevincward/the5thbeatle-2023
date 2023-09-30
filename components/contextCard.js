435aa8824fc41ba9f62cd94ef20c6532d162f08e
Tim Loden <tim.m.loden@gmail.com>
2023-09-30 13:57:53 -0700

    added context block component

---
+++ components/contextCard.js
@@ -0,0 +1,67 @@
+export default function ContextCard(props) {
+    const { titleColor, textColor, backgroundColor, content } = props;
+
+    return (
+        <div className='container py-6'>
+            <div
+                className='rounded-lg p-8'
+                style={{ backgroundColor: backgroundColor }}
+            >
+                {content.kicker && content.kicker !== '' ? (
+                    <p
+                        style={{ color: titleColor }}
+                        className='pb-6 text-xs uppercase'
+                    >
+                        {content.kicker}
+                    </p>
+                ) : (
+                    ''
+                )}
+
+                <p className='pb-6 text-lg ' style={{ color: textColor }}>
+                    {content.description}
+                </p>
+                <div className='flex-column flex flex-wrap lg:flex-row'>
+                    <div className='basis-full lg:basis-1/2'>
+                        <div className='pb-6 lg:pr-6'>
+                            <p className='pb-2' style={{ color: titleColor }}>
+                                My Role
+                            </p>
+                            <p
+                                className='pb-1  text-sm lg:text-base'
+                                style={{ color: textColor }}
+                            >
+                                {content.title}
+                            </p>
+                            <p className='text-xs text-gray'>
+                                {content.titleDescription}
+                            </p>
+                        </div>
+                    </div>
+                    <div className='basis-full lg:basis-1/4'>
+                        <p className='pb-2' style={{ color: titleColor }}>
+                            Teammates
+                        </p>
+                        <p
+                            className='pb-2 text-sm lg:text-base'
+                            style={{ color: textColor }}
+                        >
+                            {content.teammates}
+                        </p>
+                    </div>
+                    <div className='basis-full lg:basis-1/4'>
+                        <p className='pb-2' style={{ color: titleColor }}>
+                            Timeline
+                        </p>
+                        <p
+                            className=' text-sm lg:text-base'
+                            style={{ color: textColor }}
+                        >
+                            {content.timeline}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- pages/case-studies/components.js
+++ pages/case-studies/components.js
@@ -1,5 +1,6 @@
 import TitleBlock from '@/components/titleBlock';
 import ParallaxBlock from '@/components/parallaxBlock';
+import ContextCard from '@/components/contextCard';
 
 export default function Components() {
     return (
@@ -9,6 +10,21 @@ export default function Components() {
                 title='Course correcting a product to drive a culture of continuous improvement'
             />
             <ParallaxBlock image='/images/parallax-background.jpg' />
+            <ContextCard
+                titleColor='#7C4D0F'
+                textColor='#060125'
+                backgroundColor='#FDEBD2'
+                content={{
+                    kicker: 'ABOUT REFLEKTIVE',
+                    description:
+                        'Reflektive is an HR tech company that built an employee performance tracking platform for HRBPs, Managers, and coworkers- a single place to collect and provide feedback, run 1:1s, and facilitate reviews. Reflektive prioritizes peer to peer feedback to drive performance monitoring, keep users engaged, and better connect coworkers.',
+                    title: 'Senior Product Designer',
+                    titleDescription:
+                        'Product thinking, Information architecture, Visual design & Interaction, prototyping & testing',
+                    teammates: 'Product manager, Product marketing manager',
+                    timeline: 'Nov 2020 - Jan 2021',
+                }}
+            />
         </>
     );
 }
