385d2e24667b3535ab06e90101a81f452a473edb
Tim Loden <tim.m.loden@gmail.com>
2023-10-01 21:39:31 -0700

    added meta to case study

--- pages/case-studies/trifecta.js
+++ pages/case-studies/trifecta.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import TitleBlock from '@/components/titleBlock';
 import ParallaxBlock from '@/components/parallaxBlock';
 
 export default function Trifecta() {
     return (
         <>
+            <Head>
+                <title>Trifecta Nutrition Case Study</title>
+            </Head>
             <TitleBlock
                 kicker='It was a mess'
                 title='Cleaning up the Trifecta shitshow'
