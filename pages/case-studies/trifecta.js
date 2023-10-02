af6c7577b6cafec8134d797486c18a40dd1a9ef4
Kevin Ward <wheresmycanvas@gmail.com>
2023-10-02 16:35:28 -0700

    Updates to About page, Homepage, and case studies

--- pages/case-studies/trifecta.js
+++ pages/case-studies/trifecta.js
@@ -96,7 +96,7 @@ export default function Trifecta() {
                         desktopHeight: 230,
                         mobileWidth: 382,
                         mobileHeight: 230,
-                        caption: 'Product design documentation in Confluence',
+                        caption: 'Product design documentation',
                     },
                 ]}
             />
@@ -229,8 +229,8 @@ export default function Trifecta() {
             <TextBlockOne
                 images={[
                     {
-                        desktop: '/images/text-block-two-image.svg',
-                        mobile: '/images/text-block-two-image.svg',
+                        desktop: 'trifecta-featured-work-3.gif',
+                        mobile: 'trifecta-featured-work-3.gif',
                         alt: 'Mobile cart expansion animation',
                         desktopWidth: 1040,
                         desktopHeight: 230,
