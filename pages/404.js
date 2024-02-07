8b45b6d534fa479af497ebf2053ae5ffc4f78a88
Kevin Ward <wheresmycanvas@gmail.com>
2024-02-07 14:42:25 -0800

    Fixes to 404 pages

--- pages/404.js
+++ pages/404.js
@@ -6,7 +6,7 @@ export default function Custom404() {
         <>
             <div className='pt-10'></div>
             <div className='pl-3 lg:container lg:pl-0'>
-                <p className='flex items-center justify-center font-recoleta text-3xl font-semibold'>You've discovered a hidden track</p>
+                <p className='flex items-center justify-center font-recoleta text-3xl font-semibold'>You&lsquo;ve discovered a hidden track</p>
             </div>
 
 
