382240363585f9db10b55de744dc7636a314360b
Kevin Ward <wheresmycanvas@gmail.com>
2024-02-07 14:46:04 -0800

    Fixing 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Spline from '@splinetool/react-spline';
+import Link from 'next/link';
 
 export default function Custom404() {
     return (
@@ -7,6 +8,9 @@ export default function Custom404() {
             <div className='pt-10'></div>
             <div className='pl-3 lg:container lg:pl-0'>
                 <p className='flex items-center justify-center font-recoleta text-3xl font-semibold'>You&lsquo;ve discovered a hidden track</p>
+                <Link href="/">
+                    <a>homepage</a>
+                </Link>
             </div>
 
 
